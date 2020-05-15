57d4ac05ca28610ad905697ffa418088e83c8ae1
AnuragDhiman400 <anuragdhiman400@gmail.com>
2020-05-15 01:15:11 -0400

    Restful-service-springboot

---
+++ src/main/java/com/anurag/rest/basic/auth/BasicAuthenticationController.java
@@ -0,0 +1,17 @@
+package com.anurag.rest.basic.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@CrossOrigin(origins="http://todoapp-angular.s3-website-us-east-1.amazonaws.com")
+@RestController
+public class BasicAuthenticationController {
+
+	@GetMapping(path = "/basicauth")
+	public AuthenticationBean helloWorldBean() {
+		//throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
+		return new AuthenticationBean("You are authenticated");
+	}	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'anurag', 'Learn Spring Boot', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'anurag', 'Learn Angular', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'anurag', 'Learn JWT', sysdate(), false);
